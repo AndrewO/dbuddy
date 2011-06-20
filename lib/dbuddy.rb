55e651f79f4e363705f2e55a13c1d5b019b158f8
Andrew O'Brien <andrew@econify.com>
2011-06-20 13:24:11 -0400

    Fixed bug with executing multiple SQL statements; version bump

--- lib/dbuddy.rb
+++ lib/dbuddy.rb
@@ -1,9 +1,23 @@
+begin
+  require 'active_record'
+  class ActiveRecord::ConnectionAdapters::MysqlAdapter
+    def allow_multi_statements!
+      @connection.set_server_option(Mysql::OPTION_MULTI_STATEMENTS_ON)
+    end
+  end
+rescue LoadError
+end
+
 module DBuddy
   def self.run(connection, *paths)
+    connection.reconnect!
+    ActiveRecord::Base.connection.allow_multi_statements!
+
     paths.each do |path|
       puts "Processing #{path}"
       begin
         connection.execute(File.read(path), "DBuddy: Reloading #{path}")
+        puts "Done"
       rescue => e
         puts "Caught error"
         puts e.message
--- lib/dbuddy/version.rb
+++ lib/dbuddy/version.rb
@@ -1,3 +1,3 @@
 module DBuddy
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 end
